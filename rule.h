88e41439b36b1dbd7a642e17c410fb0129235138
cheney <17231055@buaa.edu.cn>
2020-05-19 13:48:53 +0800

    完成用户态内核态增删改查接口,修改了小bug：iphdr改为iphdrNow

--- rule.h
+++ rule.h
@@ -1,6 +1,9 @@
 #ifndef RULE_H
 #define RULE_H
 
+// *************
+// * rule info *
+// *************
 // 可匹配字符串长度
 #define STRPATSIZE 10
 // target 标记
@@ -45,6 +48,27 @@ struct rule
     int strFlag;
     char strPattern[STRPATSIZE + 1];
 };
-
 #define RULESIZE (sizeof(struct rule))
+
+// ***************
+// *  ctrlHeader *
+// ***************
+#define CT_INSERT 1 // len为处理个数，后面跟len大小的rule数组。在头部一次性插入多条。不会把数组逆序。
+#define CT_APPEND 2 // len为处理个数，后面跟len大小的rule数组
+#define CT_DELETE 3 // len表示要删除的序号
+#define CT_FLUSH 4  // 内核处理时忽略len
+#define CT_DTAGET 5 // 默认黑白名单。len取值为RU_ACCEPT或RU_DROP
+
+struct ctrlheader
+{
+    int ctrlType;
+    int len;
+};
+#define CTRLHDRSIZE (sizeof(struct ctrlheader))
+
+
+// **************
+// * other info *
+// **************
+#define devName "/dev/ckq"
 #endif
